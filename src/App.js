374791d77f06cbc3e013d459a9276a790e76975f
N Sony <71254514+SONY078@users.noreply.github.com>
2022-07-07 12:48:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 // import {BrowserRouter , Routes , Route} from 'react-router-dom
 import Greetings from './IDay1/iday-1'
+
+import Parent from './IDAY2/Parent';
+import Week from './IDAY2/Week';
+import Store from './IDAY2/iday-2';
+
 function App() {
   return (
     <div className="App">
+    
       <Greetings/>
+    
+      <Parent/>
+      <Store/>
+      <Week day="thursday"/>
     </div>
   );
 }
