ee1bfd7a55a0d734a901cb50e8475fa2bf0bede3
N Sony <71254514+SONY078@users.noreply.github.com>
2022-07-11 16:54:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,16 @@ import Iday3 from './IDAY3/iday-3';
 import UseEffect from './IDAY3/UseEffect';
 import UseS from './IDAY3/UseS';
 
+import LifeCycle from './IDAY4/LifeCycle';
+import {useSelector,useDispatch} from 'react-redux'
+import {Dec,Inc,Res} from './IDAY4/action';
+import CalculateFactorial from './IDAY4/UseMemoExample';
+import RenderCounter from './IDAY4/UseRef';
+import StopWatch from './IDAY4/UseRefExample';
+
 function App() {
+  const ChangeState = useSelector((state)=>state.Changer);
+  const disp = useDispatch();
   return (
     <div className="App">
     <BrowserRouter>
@@ -38,6 +47,14 @@ function App() {
           <Route path="/notfound" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <LifeCycle/>
+      <h1>{ChangeState}</h1>
+      <button onClick={()=>{disp(Inc())}}>Increment ⤴️</button>&emsp;
+      <button onClick={()=>{disp(Dec())}}>decrement ⤵️</button>&emsp;
+      <button onClick={()=>{disp(Res())}}>Reset 🔁</button>
+      <CalculateFactorial/>
+      <RenderCounter/>
+      <StopWatch/>
     </div>
   );
 }
